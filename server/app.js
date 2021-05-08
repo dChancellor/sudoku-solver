8a1924f31138f8e20b8dfd5454c57daf4ca4f179
Derek Holtzman <derekholtzman@gmail.com>
2021-05-07 20:47:47 -0500

    Complicated the express server

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+const $ = require('cheerio');
+
+const app = express();
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+const middlewares = require('./middleware');
+
+const getPuzzles = async () => {
+    const url = `https://www.nytimes.com/puzzles/sudoku/easy`;
+    let { data: html } = await axios.get(url);
+    let $parse = $.load(html);
+    let windowString = $parse('script').get()[0].children[0].data;
+    return JSON.parse(
+      windowString.substr(windowString.indexOf('{'), windowString.length)
+    );
+  };
+
+app.get('/', async (req, res) => {
+    const data = await getPuzzles();
+    res.send(data);
+})
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,24 +1,8 @@
-const express = require('express');
-const axios = require('axios');
-const $ = require('cheerio');
+const app = require('./app');
 
-const app = express();
-
-const getPuzzles = async () => {
-    const url = `https://www.nytimes.com/puzzles/sudoku/easy`;
-    let { data: html } = await axios.get(url);
-    let $parse = $.load(html);
-    let windowString = $parse('script').get()[0].children[0].data;
-    return JSON.parse(
-      windowString.substr(windowString.indexOf('{'), windowString.length)
-    );
-  };
-
-app.get('/', async (req, res) => {
-    const data = await getPuzzles();
-    res.send(data);
-})
-
-app.listen(3000, () => console.log('Server running on port 3000!'))
-
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
