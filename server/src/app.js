0a1b260beb9341c4c8ea377f0bf2569ff2fc9201
Derek Holtzman <derekholtzman@gmail.com>
2021-05-08 15:16:08 -0500

    Testing by breaking

--- server/src/app.js
+++ server/src/app.js
@@ -26,7 +26,7 @@ const getPuzzles = async () => {
   };
 
 app.get('/', async (req, res) => {
-    res.send({message: '🚀'});
+    res.send({message: '🐱‍🏍'});
 })
 
 app.get('/sudoku', async (req, res) => {
