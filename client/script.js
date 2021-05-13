8c7c11f46ffd84347088235fb92f8a659f8b8158
Derek Holtzman <derekholtzman@gmail.com>
2021-05-12 20:15:32 -0500

    Renamed variable for env variable context

--- client/script.js
+++ client/script.js
@@ -14,7 +14,7 @@ const buttonClick = (inputDifficulty) => {
 
 const getPuzzles = () => {
   document.querySelector('.loading').style.display = 'block';
-  return fetch(apiURL)
+  return fetch(API_URL)
     .then((response) => response.json())
     .then((data) => {
       document.querySelector('.loading').style.display = 'none';
