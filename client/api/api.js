057baa8dbab4d668901a0eb30e2e480ef9d5a821
Derek Holtzman <derekholtzman@gmail.com>
2021-05-15 03:12:01 -0500

    Removed document calls from serverless function

--- client/api/api.js
+++ client/api/api.js
@@ -1,9 +1,7 @@
 const getPuzzles = () => {
-  document.querySelector('.loading').style.display = 'block';
   return fetch(API_URL)
     .then((response) => response.json())
     .then((data) => {
-      document.querySelector('.loading').style.display = 'none';
       return data;
     });
 };
