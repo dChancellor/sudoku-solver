5f689522e7fd706bcc9cfde9024b839a7aab99d7
Derek Holtzman <derekholtzman@gmail.com>
2021-05-15 03:27:08 -0500

    Fixing API call

--- client/api/api.js
+++ client/api/api.js
@@ -1,16 +1,17 @@
 const https = require('https');
 
-const options = {
-  hostname: API_URL,
-  port: 443,
-  path: '/nyt',
-  method: 'GET',
-};
-
 const getPuzzles = () => {
-  return https.request(options, (res) => {
-    console.log(res.json);
-  });
+  return https.request(
+    {
+      hostname: API_URL,
+      port: 443,
+      path: '/nyt',
+      method: 'GET',
+    },
+    (res) => {
+      console.log(res.json);
+    }
+  );
 };
 
 module.exports = getPuzzles;
