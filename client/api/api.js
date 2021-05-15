8cbfa0ff8702e619e7bce5d06651cd828a3c4009
Derek Holtzman <derekholtzman@gmail.com>
2021-05-15 03:34:45 -0500

    Fixing API call

--- client/api/api.js
+++ client/api/api.js
@@ -1,17 +1,24 @@
 const https = require('https');
 
 const getPuzzles = () => {
-  return https.request(
-    {
-      hostname: process.env.API_URL,
-      port: 443,
-      path: '/nyt',
-      method: 'GET',
-    },
-    (res) => {
-      console.log(res.json);
-    }
-  );
+  let res = https
+    .get(process.env.API_URL, (resp) => {
+      let data = '';
+
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        return data;
+      });
+    })
+    .on('error', (err) => {
+      console.log('Error: ' + err.message);
+    });
+  return res;
 };
 
 module.exports = getPuzzles;
